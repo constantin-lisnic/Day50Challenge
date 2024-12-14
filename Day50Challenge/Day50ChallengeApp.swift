20c75716afb4f933f48e35566b2637971df7eb4d
Constantin Lisnic <constantin@lisnic.dev>
2024-12-14 19:05:47 +0000

    Initial Commit

---
+++ Day50Challenge/Day50ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day50ChallengeApp.swift
+//  Day50Challenge
+//
+//  Created by Constantin Lisnic on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Day50ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day50ChallengeTests/Day50ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Day50ChallengeTests.swift
+//  Day50ChallengeTests
+//
+//  Created by Constantin Lisnic on 14/12/2024.
+//
+
+import Testing
+@testable import Day50Challenge
+
+struct Day50ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
